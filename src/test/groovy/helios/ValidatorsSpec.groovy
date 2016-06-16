eb1e6639e0fbdf4fd04916a05bb1b359842d9f47
Mario Garcia <mario.ggar@gmail.com>
2016-06-16 08:58:04 +0200

    Add positive() validator spec

--- src/test/groovy/helios/ValidatorsSpec.groovy
+++ src/test/groovy/helios/ValidatorsSpec.groovy
@@ -1,5 +1,9 @@
 package helios
 
+import static helios.Validators.positive
+import static helios.Helios.validate
+
+import spock.genesis.Gen
 import spock.lang.Specification
 
 import helios.CommonProperties as PROPERTY_COMMON
@@ -12,5 +16,24 @@ class ValidatorsSpec extends Specification {
 
         expect: 'constructor to be one'
         PROPERTY_COMMON.classHasNoPublicConstructor validatorsClass
+    }
+
+    void 'check positive validator'() {
+        expect: """
+        the validator will add an error for zero or any number less
+        than zero
+        """
+        (x <= 0 && errors == 1) || true
+
+        and: """
+        it won't add any error if the value is null
+        """
+        (x > 0 && errors == 0) || true
+
+        where: "possible integer values are"
+        x << Gen.getInteger().take(100)
+
+        and: 'number of errors produced'
+        errors = validate("x", x, positive()).size()
     }
 }
